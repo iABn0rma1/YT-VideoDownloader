ff89cc9fa9988bb7799eaf47c9f89a570e9f1767
iABn0rma1 <amanbarthwal0110@gmail.com>
2024-05-29 01:15:59 +0530

    deployed

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, request, render_template, jsonify
-import os
+from flask import Flask, request, render_template, jsonify, send_file
 import yt_dlp
+import os
+import tempfile
 
 app = Flask(__name__)
 
@@ -25,15 +26,13 @@ def fetch_info():
 @app.route('/download', methods=['POST'])
 def download():
     video_url = request.form.get('video_url')
+    video_title = request.form.get('video_title')
     if not video_url:
         return jsonify({'error': "No URL provided."})
     try:
-        if 'playlist' in video_url:
-            download_youtube_playlist(video_url)
-            return jsonify({'message': "Playlist download completed!"})
-        else:
-            download_youtube_video(video_url)
-            return jsonify({'message': "Video download completed!"})
+        temp_dir = tempfile.mkdtemp()
+        file_path = download_youtube_video(video_url, video_title, temp_dir)
+        return send_file(file_path, as_attachment=True)
     except Exception as e:
         return jsonify({'error': str(e)})
 
@@ -48,31 +47,18 @@ def fetch_video_info(video_url):
             info['n_entries'] = len(info['entries'])
     return info
 
-def download_youtube_video(video_url):
+def download_youtube_video(video_url, video_title, temp_dir):
     try:
         ydl_opts = {
             'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
-            'outtmpl': os.path.join(get_user_downloads_path(), '%(title)s.%(ext)s')
+            'outtmpl': os.path.join(temp_dir, f'{video_title}.%(ext)s')
         }
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             ydl.download([video_url])
+        downloaded_file = os.listdir(temp_dir)[0]
+        return os.path.join(temp_dir, downloaded_file)
     except Exception as e:
         raise e
-
-def download_youtube_playlist(playlist_url):
-    try:
-        ydl_opts = {
-            'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
-            'outtmpl': os.path.join(get_user_downloads_path(), '%(playlist_index)s - %(title)s.%(ext)s'),
-            'noplaylist': False  # Ensure that the entire playlist is downloaded
-        }
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([playlist_url])
-    except Exception as e:
-        raise e
-
-def get_user_downloads_path():
-    return os.path.join(os.path.expanduser('~'), 'Downloads')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
---
+++ readme.md
@@ -0,0 +1,58 @@
+# YouTube Videos Downloader
+
+Welcome to the YouTube Video Downloader project! This simple web application allows users to download YouTube videos and playlists with ease. Check out the live demo [here](https://bit.ly/yt-dlp-mp4).
+
+## Features
+
+- Download individual YouTube videos or entire playlists.
+- Retrieve video information such as title and thumbnail before downloading.
+- Directly save downloaded videos to your local system.
+
+## Technologies Used
+
+- Flask: Python web framework for building the server-side application.
+- YouTube-DL: Python library for downloading YouTube videos and playlists.
+- HTML/CSS/JavaScript: Front-end technologies for building the user interface.
+- Bootstrap: Front-end framework for styling and layout.
+
+## Setup
+
+1. Clone the repository:
+
+   ```
+   git clone https://github.com/your-username/youtube-video-downloader.git
+   ```
+
+2. Install dependencies:
+
+   ```
+   pip install -r requirements.txt
+   ```
+
+3. Run the Flask application:
+
+   ```
+   python app.py
+   ```
+
+4. Access the application in your web browser at `http://localhost:5000`.
+
+## Usage
+
+1. Enter the URL of the YouTube video or playlist you want to download.
+2. Click the "Submit" button to fetch video information.
+3. Review the video details and click the "Download" button to save the video to your system.
+
+## Screenshots
+
+![alt text](assets/images/image-1.png)
+*Enter the URL of the YouTube video or playlist.*
+
+![alt text](assets/images/image-2.png)
+*Review video information before downloading.*
+
+## Contributing
+
+Contributions are welcome! If you encounter any issues or have suggestions for improvements, feel free to open an issue or submit a pull request.
+
+---
